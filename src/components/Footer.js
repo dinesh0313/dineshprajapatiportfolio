c3ad66f1d936ba4638ceaff00a4d8ce1063fd889
dinesh prajapati <prajapatidinesh313@gmail.com>
2024-01-18 20:29:32 +0530

    dineshprajapatiportfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import "./FooterStyles.css"
 
 import React from 'react'
 import { FaFacebook, FaHome, FaLinkedin, FaMailBulk, FaPhone, FaTwitter } from "react-icons/fa";
+// import pro1 from"https://www.linkedin.com/in/dinesh-prajapati-8899ba22a/"
 
 
 const Footer = () => {
@@ -29,7 +30,7 @@ const Footer = () => {
                 <h4>About me</h4>
                 <p>This is me Dinesh Prjapati.Full Stack Developer || MERN Stack || CSE'25 || JAVA || DSA || GFG || Python || C </p>
                 <div className="social">
-                    <FaFacebook size={30} style={{color:"#fff", marginRight:"1rem"}}
+                    <FaFacebook className="fc" url="https://youtube.com/shorts/_G64ohDhyw0?si=pnnpqPEs4QdAXvyB" size={30} style={{color:"#fff", marginRight:"1rem"}}
                     />
                     <FaLinkedin size={30} style={{color:"#fff", marginRight:"1rem"}}
                     />
