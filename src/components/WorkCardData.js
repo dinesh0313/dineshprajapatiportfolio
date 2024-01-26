d4d528eab0ca81702f8f756d3b5f7270ed22c293
dinesh prajapati <prajapatidinesh313@gmail.com>
2024-01-26 19:55:47 +0530

    initial commit

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,30 +1,30 @@
 import pro15 from "../assets/projectImg15jpg.png"
-import pro4 from "../assets/projectImg4.jpg"
+import pro4 from "../assets/projectImg5.jpg"
 import pro3 from "../assets/projectImg3.jpg"
 
 const ProjectCardData =[
     {
         imgsrc: pro15,
         title : "Ecommerce Website Design",
-        text: "Thriving Facebook community and top-ranked padcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! I really can so this'  for over 100,000 online entrepreneurs. Best-selling marketing courses",
+        text: "Build a basic Flipkart clone with HTML, CSS, and JavaScript. Organize files, create a responsive layout with headers, navigation, and product displays. Mimic Flipkart's design, including a search bar. Utilize HTML and CSS for product listings, add JavaScript for dynamic product details. Implement a simple shopping cart feature. Test thoroughly for a smooth user experience. Note: Backend features are excluded for simplicity.",
         view : "https://dinesh0313.github.io/Flipkart/",
         source : "https://github.com/dinesh0313/Flipkart"
     },
 
     {
         imgsrc: pro4,
-        title : "Ecommerce Website Design",
-        text: "Thriving Facebook community and top-ranked padcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! I really can so this'  for over 100,000 online entrepreneurs. Best-selling marketing courses",
-        view : "https://dinesh0313.github.io/Flipkart/",
-        source : "https://dinesh0313.github.io/Flipkart/"
+        title : "YouTube Video Downloader",
+        text: "I developed an advanced YouTube video and playlist downloader that offers higher download quality. The project leverages the pyTube Python library and incorporates various Python functionalities, resulting in a highly useful tool. Through this endeavor, I aimed to enhance the downloading experience for users by ensuring superior quality and utilizing efficient programming techniques. The project also includes a user-friendly interface.",
+        view : "https://github.com/dinesh0313/youtube_video_playlist_downloader",
+        source : "https://github.com/dinesh0313/youtube_video_playlist_downloader"
     },
 
     {
         imgsrc: pro3,
-        title : "Ecommerce Website Design",
-        text: "Thriving Facebook community and top-ranked padcast, Amy inspires a grounded, tangible and self-affirming sense of 'Wow! I really can so this'  for over 100,000 online entrepreneurs. Best-selling marketing courses",
-        view : "https://dinesh0313.github.io/Flipkart/",
-        source : "https://dinesh0313.github.io/Flipkart/"
+        title : "Dance Website Design",
+        text: "Designed a dynamic dance website using Pug, HTML, CSS, and JavaScript. Implemented backend functionality with Node.js and Express.js, ensuring seamless server-client communication. Integrated MongoDB for efficient data storage, enabling the website to manage dance content and user interactions effectively. This project harmoniously combines frontend and backend.",
+        view : "https://github.com/dinesh0313/Dance-Website",
+        source : "https://github.com/dinesh0313/Dance-Website"
     }
 ]
 
